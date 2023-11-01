ab8d368cf72e98139745a50d37eeb63ad9a5e1e0
nandailhamfadlika <nandailhamfadlika@gmail.com>
2023-11-01 10:28:26 +0700

    Tugas DDP 6

---
+++ Pertemuan 6/Praktikum6.py
@@ -0,0 +1,14 @@
+for i in range(51,0,-5):
+    print(i)
+
+
+for e in range(3):
+    print("nanda")
+
+print()
+
+for x in range (1,11):
+    for j in range(1,11):
+        k=x*j
+        print(k,end='')
+    print()
